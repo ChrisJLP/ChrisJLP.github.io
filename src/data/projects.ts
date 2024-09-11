5899e89ea8ff359a8d5654963ab7396466cf9a03
Chris Pring <chrisjlpring@outlook.com>
2024-09-11 15:50:13 +0100

    Create project type and data

---
+++ src/data/projects.ts
@@ -0,0 +1,9 @@
+import { Project } from '../types/Project';
+
+export const projects: Project[] = [
+  {
+    id: '1',
+    title: 'Project 1',
+    description: 'Description for Project 1',
+  },
+];
