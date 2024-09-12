4c276e442dc6d43245736c0b3ef5f26156df342c
Chris Pring <chrisjlpring@outlook.com>
2024-09-12 16:22:12 +0100

    Add project image to public/assets so both local and distributed can access

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,6 @@ export const projects: Project[] = [
     id: '1',
     title: 'SimpliStock',
     description: 'A purchasing web app designed to showcase how a purchasing system could be made as easy to use as possible. Features real-time stock updates, order processing, and supplier management.',
-    imageUrl: 'dist/assets/simpliStockDashboard.jpeg',
+    imageUrl: 'assets/SimpliStockDashboard.jpeg',
   },
 ];
