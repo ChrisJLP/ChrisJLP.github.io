11080c0273f90a92d2a0f7108f9cccfbf13da20a
Chris Pring <chrisjlpring@outlook.com>
2024-11-18 16:15:57 +0000

    Deploy updated build to GitHub Pages

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a project to create my personal website, while learning how to use TypeScript and Tailwind CSS.
--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-  <footer>
-    {/* Add footer content here */}
-  </footer>
-);
-
-export default Footer;
